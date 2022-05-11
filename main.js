5fd363c93258b992565ebcdb0d18c68fde3272ef
katy4599 <katy.millard@gmail.com>
2022-05-11 16:34:41 -0500

    for real this time

--- main.js
+++ main.js
@@ -18,44 +18,44 @@ const pselect = document.getElementById("pselect");
               
               switch (event.target.value) {
                   case 'rock':
-                      playerImage.src = '.rock-paper-scissors/rock.jpg';
+                      playerImage.src = '.rock.jpg';
                       if (compInput === 0) {
-                          compImage.src = '.rock-paper-scissors/rock.jpg';
-                          winImage.src = '.rock-paper-scissors/draw.jpg';
+                          compImage.src = '.rock.jpg';
+                          winImage.src = '.draw.jpg';
                           winner.innerHTML = "It's a draw!";
                           draws.innerText = Number(draws.innerText)+1;
                           window.sessionStorage.setItem("draws.innerText", draws.innerText.toString());
                         } else if (compInput === 1) {
-                            compImage.src = '.rock-paper-scissors/paper.jpg';
-                            winImage.src = '.rock-paper-scissors/papercoversrock.jpg';
+                            compImage.src = '.paper.jpg';
+                            winImage.src = '.papercoversrock.jpg';
                             winner.innerHTML = "Try Again!";
                             losses.innerText = Number(losses.innerText)+1;
                             window.sessionStorage.setItem("losses.innerText", losses.innerText.toString());
                         } else {
-                            compImage.src = '.rock-paper-scissors/scissors.jpg';
-                            winImage.src = '.rock-paper-scissors/rockbeatsscissors.jpg';
+                            compImage.src = '.scissors.jpg';
+                            winImage.src = '.rockbeatsscissors.jpg';
                             winner.innerHTML = "Player Wins!";
                             wins.innerText = Number(wins.innerText)+1;
                             window.sessionStorage.setItem("wins.innerText", wins.innerText.toString());
                         }
                         break;
                         case 'paper':
-                            playerImage.src = '.rock-paper-scissors/paper.jpg';
+                            playerImage.src = '.paper.jpg';
                         if (compInput === 0) {
-                            compImage.src = '.rock-paper-scissors/rock.jpg';
-                            winImage.src = '.rock-paper-scissors/papercoversrock.jpg';
+                            compImage.src = '.rock.jpg';
+                            winImage.src = '.papercoversrock.jpg';
                             winner.innerHTML = "Player Wins!";
                             wins.innerText = Number(wins.innerText)+1;
                             window.sessionStorage.setItem("wins.innerText", wins.innerText.toString());
                         } else if (compInput === 1) {
-                            compImage.src = '.rock-paper-scissors/paper.jpg';
-                            winImage.src = '.rock-paper-scissors/draw.jpg';
+                            compImage.src = '.paper.jpg';
+                            winImage.src = '.draw.jpg';
                             winner.innerHTML = "It's a draw!";
                             draws.innerText = Number(draws.innerText)+1;
                             window.sessionStorage.setItem("draws.innerText", draws.innerText.toString());
                         } else {
-                            compImage.src = '.rock-paper-scissors/scissors.jpg';
-                            winImage.src = '.rock-paper-scissors/scissorsbeatpaper.jpg';
+                            compImage.src = '.scissors.jpg';
+                            winImage.src = '.scissorsbeatpaper.jpg';
                             winner.innerHTML = "Try Again!";
                             losses.innerText = Number(losses.innerText)+1;
                             window.sessionStorage.setItem("losses.innerText", losses.innerText.toString());
@@ -70,14 +70,14 @@ const pselect = document.getElementById("pselect");
                                 losses.innerText = Number(losses.innerText)+1;
                                 window.sessionStorage.setItem("losses.innerText", losses.innerText.toString());
                             } else if (compInput === 1) {
-                                compImage.src = '.rock-paper-scissors/paper.jpg';
-                                winImage.src = '.rock-paper-scissors/scissorsbeatpaper.jpg';
+                                compImage.src = '.paper.jpg';
+                                winImage.src = '.scissorsbeatpaper.jpg';
                                 winner.innerHTML = "Player Wins!";
                                 wins.innerText = Number(wins.innerText)+1;
                                 window.sessionStorage.setItem("wins.innerText", wins.innerText.toString());
                             } else {
-                                compImage.src = '.rock-paper-scissors/scissors.jpg';
-                                winImage.src = '.rock-paper-scissors/draw.jpg';
+                                compImage.src = '.scissors.jpg';
+                                winImage.src = '.draw.jpg';
                                 winner.innerHTML = "It's a draw!";
                                 draws.innerText = Number(draws.innerText)+1;
                                 window.sessionStorage.setItem("draws.innerText", draws.innerText.toString());
